bdda2b1de443236b41d4fc1b1d9c52e3df7d70bd
AoZora3 <qfvescarnuela@tip.edu.ph>
2026-09-04 21:06:24 +0800

    Update selection sort to use Comparable for comparisons
    Refactor comparison logic to use Comparable interface directly.

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,10 +2,11 @@ public class SelectionSort<T extends Comparable<T>> {
     public void sort(SortValue<T>[] arr, boolean ascending, boolean visualize) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
+            // Scan the unsorted suffix for the next smallest or largest value, depending on direction.
             int minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if ((ascending && arr[j].getValue().compareTo(arr[minIndex].getValue()) < 0) ||
-                    (!ascending && arr[j].getValue().compareTo(arr[minIndex].getValue()) > 0)) {
+                if ((ascending && arr[j].compareTo(arr[minIndex]) < 0) ||
+                    (!ascending && arr[j].compareTo(arr[minIndex]) > 0)) {
                     minIndex = j;
                 }
             }
@@ -15,7 +16,7 @@ public class SelectionSort<T extends Comparable<T>> {
                 arr[i] = temp;
             }
             if (visualize) {
-                Visualizer.visualizeSort(arr, "Selection Sort - Step " + (i + 1));
+                Visualizer.visualizeSort(arr, "Selection Sort - Step " + (i + 1), i);
             }
         }
     }
